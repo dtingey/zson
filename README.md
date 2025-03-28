9a03ce2c117fb9d84428be5242fe83cf04145370
dtingey <damon.tingey@gmail.com>
2025-03-28 09:59:57 -0600

    build

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Zig Python Library for streaming to and from Json
 
 
 Uses Zig version 0.14.0 and ziggy-pydust for Python bindings
+
+!!! WIP. May be broken. !!!
--- src/json_generator.zig
+++ src/json_generator.zig
@@ -1,1 +1,7 @@
+//! Library for streaming data to json.
 const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    print("Hello, world!\n", .{});
+}
